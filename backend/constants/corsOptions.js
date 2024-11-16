a011884297ba93a494c9610d508453470f139c23
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-11-16 17:18:40 +0530

    added env for cors

--- backend/constants/corsOptions.js
+++ backend/constants/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: process.env.FRONTEND_URL, 
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow cookies to be sent and received
