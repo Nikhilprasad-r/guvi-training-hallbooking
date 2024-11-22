5d639025a845e1117854356f4a430e7d8d94dc4e
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-11-22 13:58:13 +0530

    using sql using sequelize

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,23 @@ import express from "express";
 import cors from "cors";
 import auth from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-import connectDB from "./utils/connectDB.js";
 import hallRoutes from "./routes/halls.js";
 import corsOptions from "./constants/corsOptions.js";
+import path from 'path';
+import { connectMysqlDb } from "./utils/connectMysqlDb.js";
 
-
-
+const staticPath=path.resolve("public")
+connectMysqlDb()
 const app = express();
+
+app.use("/static",express.static(staticPath))
 
 app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
 
-connectDB();
+
 
 app.use("/api/auth", auth);
 
